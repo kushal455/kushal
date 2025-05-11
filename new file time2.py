11d768e554c14639ea7763d4011dc481e3c59cab
kushal455 <aggarwalkushal78@gmail.com>
2025-05-11 22:24:16 +0530

    Add files via upload

---
+++ new file time2.py
@@ -0,0 +1,46 @@
+# # marks=int(input("enter your marks:"))
+
+# # if  (marks>=90):
+# #     print("grade A")
+# # elif (marks>=80 and marks<90 ):
+# #     print("grade B")
+# # elif(marks>=70 and marks < 80):
+# #     print("grade C")
+
+# # else:
+# #     print("grade D")
+
+# # a
+
+# # num=int(input("enter any number :"))
+
+# # rem=num % 2
+
+
+# # if(rem==0):
+# #     print("even")
+# # else:
+# #     print("odd")
+
+# val1=int(input("enter first number :"))
+# val2=int(input("enter second number :"))
+# val3=int(input("enter third number :"))
+
+# if(val1>=val2 and val1>=val3):
+#     greater=val1
+
+# if(val2>=val1 and val2>=val3):
+#     greater=val2
+# if(val3>=val1 and val3>=val2):
+#     greater=val3
+
+# print( "greatest value is :" ,greater)
+
+mult=int(input("enter any number:"))
+
+
+if(mult % 5 ==0):
+    print("its multiple of 7")
+
+else:
+    print("its not multiple of 7")
---
+++ phython calculator.py
@@ -0,0 +1,78 @@
+# num1=float(input("enter any number: "))
+# op=(input("enter arthmetic operator : "))
+# num2=float(input("enter your third number : "))
+# if op == "+":
+#     result= num1 + num2
+
+# elif op == "-":
+#     result=num1-num2
+# elif op == "/":
+#     result=num1/num2
+
+# elif op == "*":
+#     result=num1*num2
+
+# elif op=="%":
+#     result=num1%num2
+# elif op =="**":
+#     result=num1**num2
+
+# else:
+#     result ="error detected wrong operator"
+# print("your Result is :",result)
+
+    
+# a=5
+# b=4
+# print(not False)
+
+# print(not(a>b))
+# val1=True
+# val2=True
+
+# a=  float("2")
+# b=2.5
+
+# print(type(a))
+# print(a+b)
+
+# a=int(input("enter any value :"))
+# b=int(input("enter sceond value :"))
+
+# print("avg=",(a+b/2))
+
+# a=float(input("enter any value:"))
+# b=float(input("enter second value :"))
+
+# print(a>=b)
+
+# str1=" hy buddy \n This is a string."
+
+# print(str1)
+
+# str1="apna"
+# str2="college"
+
+# print(str1+str2)
+
+# print(len(str1))
+# print(len(str2))
+# print(len(str1+str2))
+
+# str="apna college"
+# print(str.capitalize())
+# print(str)
+
+# str="hey buddy"
+# print(str.replace("dd","yy"))
+
+# str=" hello let do codeing"
+# print(str.count("let"))
+
+
+# str="hy i am the $ $ of $name"
+
+# print(str.count('$'))
+
+xxx
+
